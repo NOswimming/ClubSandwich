c79d49b3a99ab351d074c96f9c36ae522ddd2465
Mary Tionko <maryt@olympic.co.nz>
2018-02-16 14:18:24 +1300

    Adds Members page

---
+++ ClubSandwich/ClubSandwich/MembersPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClubSandwich
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MembersPage : ContentPage
+	{
+		public MembersPage ()
+		{
+			InitializeComponent ();
+        }
+
+        public void LogOut_Clicked(Object sender, ClickedEventArgs e)
+        {
+            App.Current.MainPage = new LoginPage();
+        }
+    }
+}
--- ClubSandwich/ClubSandwich/WeekPage.xaml.cs
+++ ClubSandwich/ClubSandwich/WeekPage.xaml.cs
@@ -1,17 +1,12 @@
-﻿using ClubSandwich.Service.Query;
-using ClubSandwich.ViewModel;
+﻿using ClubSandwich.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ClubSandwich
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class WeekPage : ContentPage
 	{
         public WeekPageViewModel vm;
